9b35e062a7e1fd962dc2fc8777ce8cd4f7c83dee
Lava <lava.kumar@p2epl.com>
2025-01-31 13:29:13 +0530

    modal changes

--- src/components/organisms/Card/Card.tsx
+++ src/components/organisms/Card/Card.tsx
@@ -4,9 +4,10 @@ import AllergyTable from "../AllergiesCard/AllergiesCard";
 interface CardProps {
   title: string;
   children: React.ReactNode;
+  footer?: React.ReactNode; // Add footer prop
 }
 
-const DraggableCard: React.FC<CardProps> = ({ title, children }) => {
+const DraggableCard: React.FC<CardProps> = ({ title, children, footer }) => {
   const [position, setPosition] = useState({ x: 100, y: 100 });
   const [size, setSize] = useState({ width: 700, height: 500 });
   const [isDragging, setIsDragging] = useState(false);
@@ -14,6 +15,7 @@ const DraggableCard: React.FC<CardProps> = ({ title, children }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const isResizing = useRef(false);
   const modalRef = useRef<HTMLDivElement>(null);
+
   const handleDragStart = () => {
     setIsDragging(true);
   };
@@ -21,71 +23,6 @@ const DraggableCard: React.FC<CardProps> = ({ title, children }) => {
   const handleDragEnd = () => {
     setIsDragging(false);
   };
-
-  interface Allergy {
-    id: string;
-    type: string;
-    allergen: string;
-    title: string;
-    begdate: string;
-    enddate: string;
-    diagnosis: string;
-    reaction?: string;
-    severity: string;
-  }
-
-  const allergyData: Allergy[] = [
-    {
-      id: "7162860202000553277",
-      allergen: "Pencillin",
-      type: "medication",
-      title: "ICD10:G30.0(Alzheimer's disease with early onset)",
-      // begdate: "2025-01-01 17:49:01",
-      begdate: "2025-01-01",
-      // enddate: "2025-01-31 17:49:04",
-      enddate: "2025-01-31",
-      diagnosis: "ICD10:G30.0",
-      reaction: "test Reaction",
-      severity: "255604002",
-    },
-    {
-      id: "7162860202000553295",
-      type: "food",
-      allergen: "Peanuts",
-      title:
-        "ICD10:T84.81XD(Embolism due to internal orthopedic prosthetic devices, implants and grafts, subsequent encounter)",
-      // begdate: "2025-01-01 17:50:43",
-      begdate: "2025-01-01",
-      enddate: "",
-      diagnosis: "ICD10:T84.81XD",
-      severity: "255604002",
-    },
-    {
-      id: "7162860202000553277",
-      allergen: "Pencillin",
-      type: "environmental",
-      title: "ICD10:G30.0(Alzheimer's disease with early onset)",
-      // begdate: "2025-01-01 17:49:01",
-      begdate: "2025-01-01",
-      // enddate: "2025-01-31 17:49:04",
-      enddate: "2025-01-31",
-      diagnosis: "ICD10:G30.0",
-      reaction: "test Reaction",
-      severity: "255604002",
-    },
-    {
-      id: "7162860202000553295",
-      type: "medication",
-      allergen: "Peanuts",
-      title:
-        "ICD10:T84.81XD(Embolism due to internal orthopedic prosthetic devices, implants and grafts, subsequent encounter)",
-      // begdate: "2025-01-01 17:50:43",
-      begdate: "2025-01-01",
-      enddate: "",
-      diagnosis: "ICD10:T84.81XD",
-      severity: "255604002",
-    },
-  ];
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
@@ -229,169 +166,162 @@ const DraggableCard: React.FC<CardProps> = ({ title, children }) => {
     <>
       {isModalOpen && (
         <div
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-md"
+          className="fixed inset-0 flex items-center justify-center bg-transparent bg-opacity-50 backdrop-blur-sm"
           onClick={handleCloseModal}
         >
           <div
             ref={modalRef}
             className="bg-white p-4 rounded-lg shadow-lg w-[90%] max-w-2xl"
           >
-            <div className="flex justify-between items-center border-b pb-2">
-              <span className="font-semibold">Expanded Card</span>
+            <div className="flex justify-between items-center  pb-2">
+              <span className="font-semibold">{title}</span>
               <button onClick={() => setIsModalOpen(false)}>❌</button>
             </div>
-            <div className="p-4">Modal Content</div>
+            <div className="p-4 relative">
+              {/* Render the child content (AllergyTable) inside the modal */}
+              {children}
+            </div>
+            {/* Render the footer inside the modal */}
+            {footer && <div className="border-t pt-2">{footer}</div>}
           </div>
         </div>
       )}
-      <div
-        className="absolute overflow-x-auto bg-white shadow-lg rounded-lg border border-gray-200"
-        draggable
-        onDragStart={handleDragStart}
-        onDragEnd={handleDragEnd}
-        style={{
-          width: `${size.width}px`,
-          height: isCollapsed ? "auto" : `${size.height}px`, // Adjust height when collapsed
-          left: `${position.x}px`,
-          top: `${position.y}px`,
-          cursor: isDragging ? "grabbing" : "default",
-        }}
-        onMouseDown={handleMouseDown}
-      >
-        <div className="drag-handle flex items-center justify-between p-4 cursor-move bg-white">
-          <h3 className="font-medium">{title}</h3>
-          <div className="flex items-center gap-1">
-            <button
-              onClick={handleCollapse}
-              className="p-1 hover:bg-gray-100 rounded-md transition-colors"
-            >
-              {isCollapsed ? (
-                <>
+      {!isModalOpen && (
+        <div
+          className="absolute overflow-x-auto bg-white shadow-lg rounded-lg border border-gray-200"
+          draggable
+          onDragStart={handleDragStart}
+          onDragEnd={handleDragEnd}
+          style={{
+            width: `${size.width}px`,
+            height: isCollapsed ? "auto" : `${size.height}px`,
+            left: `${position.x}px`,
+            top: `${position.y}px`,
+            cursor: isDragging ? "grabbing" : "default",
+          }}
+          onMouseDown={handleMouseDown}
+        >
+          <div className="drag-handle flex items-center justify-between p-4 cursor-move bg-white">
+            <h3 className="font-medium">{title}</h3>
+            <div className="flex items-center gap-1">
+              <button
+                onClick={handleCollapse}
+                className="p-1 hover:bg-gray-100 rounded-md transition-colors"
+              >
+                {isCollapsed ? (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke-width="1.5"
+                    strokeWidth="1.5"
                     stroke="currentColor"
-                    aria-hidden="true"
-                    data-slot="icon"
                     className="w-4 h-4"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                    ></path>
+                    />
                   </svg>
-                </>
-              ) : (
-                <>
+                ) : (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke-width="1.5"
+                    strokeWidth="1.5"
                     stroke="currentColor"
-                    aria-hidden="true"
-                    data-slot="icon"
                     className="w-4 h-4"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="m4.5 15.75 7.5-7.5 7.5 7.5"
-                    ></path>
+                    />
                   </svg>
-                </>
-              )}
-            </button>
-            <button className="p-1 hover:bg-gray-100 rounded-md transition-colors">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                data-slot="icon"
-                className="w-4 h-4"
+                )}
+              </button>
+              <button
+                onClick={handleExpandModal}
+                className="p-1 hover:bg-gray-100 rounded-md transition-colors"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
-                ></path>
-              </svg>
-            </button>
-            <button
-              className="p-1 hover:bg-gray-100 rounded-md transition-colors"
-              type="button"
-              id="radix-:r4h:"
-              aria-haspopup="menu"
-              aria-expanded="false"
-              data-state="closed"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                data-slot="icon"
-                className="w-4 h-4"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+                  />
+                </svg>
+              </button>
+              <button
+                className="p-1 hover:bg-gray-100 rounded-md transition-colors"
+                type="button"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
-                ></path>
-              </svg>
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
-        </div>
-        {/* Invisible Resize Areas */}
-        {!isCollapsed && (
-          <>
-            <div
-              className="absolute top-0 left-0 w-full h-2 cursor-ns-resize"
-              onMouseDown={(e) => handleResizeMouseDown(e, "top")}
-            />
+          {!isCollapsed && (
             <>
+              <div
+                className="absolute top-0 left-0 w-full h-2 cursor-ns-resize"
+                onMouseDown={(e) => handleResizeMouseDown(e, "top")}
+              />
               {children}
-              {/* {!isCollapsed && <>{footer}</>} */}
+              {!isCollapsed && footer && (
+                <div className="border-t p-2">{footer}</div>
+              )}
+              <div
+                className="absolute bottom-0 left-0 w-full h-2 cursor-ns-resize"
+                onMouseDown={(e) => handleResizeMouseDown(e, "bottom")}
+              />
+              <div
+                className="absolute left-0 top-0 w-2 h-full cursor-ew-resize"
+                onMouseDown={(e) => handleResizeMouseDown(e, "left")}
+              />
+              <div
+                className="absolute right-0 top-0 w-2 h-full cursor-ew-resize"
+                onMouseDown={(e) => handleResizeMouseDown(e, "right")}
+              />
+              <div
+                className="absolute top-0 left-0 w-4 h-4 cursor-nwse-resize"
+                onMouseDown={(e) => handleResizeMouseDown(e, "top-left")}
+              />
+              <div
+                className="absolute top-0 right-0 w-4 h-4 cursor-nesw-resize"
+                onMouseDown={(e) => handleResizeMouseDown(e, "top-right")}
+              />
+              <div
+                className="absolute bottom-0 left-0 w-4 h-4 cursor-nesw-resize"
+                onMouseDown={(e) => handleResizeMouseDown(e, "bottom-left")}
+              />
+              <div
+                className="absolute bottom-0 right-0 w-4 h-4 cursor-nwse-resize"
+                onMouseDown={(e) => handleResizeMouseDown(e, "bottom-right")}
+              />
             </>
-            <div
-              className="absolute bottom-0 left-0 w-full h-2 cursor-ns-resize"
-              onMouseDown={(e) => handleResizeMouseDown(e, "bottom")}
-            />
-            <div
-              className="absolute left-0 top-0 w-2 h-full cursor-ew-resize"
-              onMouseDown={(e) => handleResizeMouseDown(e, "left")}
-            />
-            <div
-              className="absolute right-0 top-0 w-2 h-full cursor-ew-resize"
-              onMouseDown={(e) => handleResizeMouseDown(e, "right")}
-            />
-            <div
-              className="absolute top-0 left-0 w-4 h-4 cursor-nwse-resize"
-              onMouseDown={(e) => handleResizeMouseDown(e, "top-left")}
-            />
-            <div
-              className="absolute top-0 right-0 w-4 h-4 cursor-nesw-resize"
-              onMouseDown={(e) => handleResizeMouseDown(e, "top-right")}
-            />
-            <div
-              className="absolute bottom-0 left-0 w-4 h-4 cursor-nesw-resize"
-              onMouseDown={(e) => handleResizeMouseDown(e, "bottom-left")}
-            />
-            <div
-              className="absolute bottom-0 right-0 w-4 h-4 cursor-nwse-resize"
-              onMouseDown={(e) => handleResizeMouseDown(e, "bottom-right")}
-            />
-          </>
-        )}
-      </div>
+          )}
+        </div>
+      )}
     </>
   );
 };
