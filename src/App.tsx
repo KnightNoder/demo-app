9c6cd1c11bc614a969ff967073d6ea418ca84c7a
EnsLapcon2 <rnalika@drcloudehr.com>
2025-02-04 14:26:49 +0530

    card with footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Provider } from "react-redux";
 import store from "./store/store";
-// import TodoList from "./components/organisms/TodoList/TodoList";
 import CardFooter from "./components/molecules/CardFooter/CardFooter";
 import Card from "./components/organisms/Card/Card";
 import DiagnosisCard from "./components/organisms/DiagnosisCard/DiagnosisCard";
 import AllergyTable from "./components/organisms/AllergiesCard/AllergiesCard";
 import "./index.css";
 
+const CardWithFooter: React.FC<{ title: string; children: ReactNode }> = ({ title, children }) => (
+  <Card title={title}>
+    {children}
+    <CardFooter />
+  </Card>
+);
+
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      {/* <TodoList /> */}
       <>
-        <Card title="Allergies">
-          <>
-            <AllergyTable />
-            <CardFooter />
-          </>
-        </Card>
+        <CardWithFooter title="Allergies">
+          <AllergyTable />
+        </CardWithFooter>
 
-        <Card title="Diagnosis">
-          <>
-            <DiagnosisCard   />
-            <CardFooter />
-          </>
-        </Card>
+        <CardWithFooter title="Diagnosis">
+          <DiagnosisCard />
+        </CardWithFooter>
       </>
     </Provider>
   );
