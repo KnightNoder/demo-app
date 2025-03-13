84f538e121b089886367597f3ed6e567f4ebd39b
EnsLapcon2 <rnalika@drcloudehr.com>
2025-03-13 13:10:10 +0530

    onAction popup modal added for modal fix

--- src/components/organisms/DisclosuresCard/DisclosuresCard.tsx
+++ src/components/organisms/DisclosuresCard/DisclosuresCard.tsx
@@ -1,11 +1,5 @@
-import React from 'react'
-
 const DisclosuresCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default DisclosuresCard
+export default DisclosuresCard;
