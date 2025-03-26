5f1679fff3bdfa182c1779537df4d9e1f955f637
EnsLapcon2 <rnalika@drcloudehr.com>
2025-03-26 12:32:26 +0530

    build fix

--- src/components/molecules/CardWithFooter/CardWithFooter.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-import Card from "../../organisms/Card/Card";
-
-export const CardWithFooter: React.FC<{ title: string; children: ReactNode, initialPosition: { x: number; y: number } }> = ({ title, children, initialPosition }) => (
-  <Card title={title} initialPosition={initialPosition}>
-    {children}
-  </Card>
-);
