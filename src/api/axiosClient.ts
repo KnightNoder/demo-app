f613bbcffe4e33762b505b49ab21e18fe8a228be
EnsLapcon2 <rnalika@drcloudehr.com>
2025-04-14 14:52:54 +0530

    changed env file,added sitename

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -4,9 +4,9 @@ const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
-    sitename: "current",
+    sitename: import.meta.env.VITE_SITE_NAME,
     Authorization: `Bearer ${localStorage.getItem("JWT_AUTH_TOKEN")}`,
-    // Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicm9sZXMiOlsiYWRtaW4iXSwiaWF0IjoxNzQ0NjE4NTgyLCJleHAiOjE3NDQ2MjIxODJ9.oaYzvjSh-_dGZvs_Ush1oMODGnMdqZqzPKgFTRZ2olg`,
+    // Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicm9sZXMiOlsiYWRtaW4iXSwiaWF0IjoxNzQ0NjIyMjIyLCJleHAiOjE3NDQ2MjU4MjJ9.--vy-iRDkdyBOrnoTgC4jNMY7tHJG_2KaIK2lp2HDcc`,
   },
 });
 
