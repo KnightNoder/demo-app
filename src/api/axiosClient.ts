0448885a7a6f2f167f4e81509fa26c299bda65e0
EnsLapcon2 <rnalika@drcloudehr.com>
2025-04-17 18:04:42 +0530

    staging changes

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -6,7 +6,11 @@ const axiosClient = axios.create({
     "Content-Type": "application/json",
     sitename: import.meta.env.VITE_SITE_NAME,
     Authorization: `Bearer ${localStorage.getItem("JWT_AUTH_TOKEN")}`,
+<<<<<<< HEAD
     // Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicm9sZXMiOlsiYWRtaW4iXSwiaWF0IjoxNzQ0ODg1NDMyLCJleHAiOjE3NDQ4ODkwMzJ9.N6E8VgKM6bch4DsykqWwKZLxIpBcVjREuYHDJWTMILk`,
+=======
+    //Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicm9sZXMiOlsiYWRtaW4iXSwiaWF0IjoxNzQ0ODE3NTYyLCJleHAiOjE3NDQ4MjExNjJ9.vcxPUcTJ1MlFboXPD1l0ZONsSxpfP0x4-1HH3w58DSc`,
+>>>>>>> 6cbf5362f3656954434bcb606c834571a28b24fc
   },
 });
 
--- src/hooks/usePermissions.ts
+++ src/hooks/usePermissions.ts
@@ -44,7 +44,11 @@ export const usePermissions = () => {
 
       try {
         const response = await fetch(
+<<<<<<< HEAD
+          `https://staging.qa-phoenix.drcloudemr.com/api/acl?username=${username}`,
+=======
           `${import.meta.env.VITE_API_URL}/acl?username=${username}`,
+>>>>>>> master
           requestOptions
         );
 
