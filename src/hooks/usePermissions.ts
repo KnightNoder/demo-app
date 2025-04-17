b22a2e86b3f264a7e2492b84d9945042fdfdf036
EnsLapcon2 <rnalika@drcloudehr.com>
2025-04-17 18:10:21 +0530

    sitename made dynamic in api.ts

--- src/hooks/usePermissions.ts
+++ src/hooks/usePermissions.ts
@@ -14,7 +14,7 @@ export const usePermissions = () => {
     const fetchInsurancePermissions = async () => {
       setLoading(true);
       const myHeaders = new Headers();
-      myHeaders.append("sitename", "current");
+      myHeaders.append("sitename", import.meta.env.VITE_SITE_NAME);
 
       const requestOptions = {
         method: "GET",
@@ -24,11 +24,11 @@ export const usePermissions = () => {
 
       const token = localStorage.getItem("JWT_AUTH_TOKEN");
       let username = "";
-      
+
       if (token) {
         try {
           const decoded = jwtDecode<DecodedToken>(token);
-          username = decoded.username || '';
+          username = decoded.username || "";
         } catch (err) {
           console.error("Error decoding JWT token:", err);
           setError("Invalid authentication token");
@@ -44,11 +44,7 @@ export const usePermissions = () => {
 
       try {
         const response = await fetch(
-<<<<<<< HEAD
-          `https://staging.qa-phoenix.drcloudemr.com/api/acl?username=${username}`,
-=======
           `${import.meta.env.VITE_API_URL}/acl?username=${username}`,
->>>>>>> master
           requestOptions
         );
 
@@ -60,9 +56,10 @@ export const usePermissions = () => {
 
         const hasPermission = result.permissions.some(
           (permission: any) =>
-            permission.section === "patients" && permission.object === "insurance"
+            permission.section === "patients" &&
+            permission.object === "insurance"
         );
-        
+
         setInsuranceWritePermission(hasPermission);
         setLoading(false);
       } catch (err) {
